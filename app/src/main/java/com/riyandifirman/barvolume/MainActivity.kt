c893ecb20700a5240d8cec0b802cc7a473b58314
riyandifirman <riyandi.firman08@gmail.com>
2023-03-02 20:56:03 +0700

    refactor findViewById with View Binding

--- app/src/main/java/com/riyandifirman/barvolume/MainActivity.kt
+++ app/src/main/java/com/riyandifirman/barvolume/MainActivity.kt
@@ -3,43 +3,46 @@ package com.riyandifirman.barvolume
 //import library yang dibutuhkan
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.riyandifirman.barvolume.databinding.ActivityMainBinding
 
 //membuat class MainActivity yang merupakan turunan dari AppCompatActivity
 //View.OnClickListener digunakan untuk mendengarkan event klik pada button
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-    //deklarasi variabel
-    private lateinit var edtWidth: EditText
-    private lateinit var edtHeight: EditText
-    private lateinit var edtLength: EditText
-    private lateinit var btnCalculate: Button
-    private lateinit var tvResult: TextView
+//    deklarasi variabel
+//    private lateinit var edtWidth: EditText
+//    private lateinit var edtHeight: EditText
+//    private lateinit var edtLength: EditText
+//    private lateinit var btnCalculate: Button
+//    private lateinit var tvResult: TextView
+
+    // deklarasi variabel binding
+    private lateinit var  binding: ActivityMainBinding
 
     //membuat fungsi onCreate yang akan dipanggil ketika activity dibuat
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //menghubungkan layout activity_main.xml dengan class MainActivity.kt
-        setContentView(R.layout.activity_main)
+        // Binding View dengan Activity Main Binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        // Memanggil fungsi setContentView untuk menampilkan layout
+        setContentView(binding.root)
 
         //menghubungkan variabel dengan id pada layout
-        edtWidth = findViewById(R.id.edt_width)
-        edtHeight = findViewById(R.id.edt_height)
-        edtLength = findViewById(R.id.edt_length)
-        btnCalculate = findViewById(R.id.btn_calculate)
-        tvResult = findViewById(R.id.tv_result)
+//        edtWidth = findViewById(R.id.edt_width)
+//        edtHeight = findViewById(R.id.edt_height)
+//        edtLength = findViewById(R.id.edt_length)
+//        btnCalculate = findViewById(R.id.btn_calculate)
+//        tvResult = findViewById(R.id.tv_result)
 
         //mendengarkan event klik pada button
-        btnCalculate.setOnClickListener(this)
+        binding.btnCalculate.setOnClickListener(this)
 
         //mengambil nilai dari savedInstanceState
         if (savedInstanceState != null) {
             //mengambil nilai dari key STATE_RESULT
             val result = savedInstanceState.getString(STATE_RESULT)
             //menampilkan nilai dari result ke TextView
-            tvResult.text = result
+            binding.tvResult.text = result
         }
     }
 
@@ -53,31 +56,31 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         //membuat kondisi ketika button diklik
         if (v?.id == R.id.btn_calculate) {
             //mengambil nilai dari EditText
-            val inputLength = edtLength.text.toString().trim()
-            val inputWidth = edtWidth.text.toString().trim()
-            val inputHeight = edtHeight.text.toString().trim()
+            val inputLength = binding.edtLength.text.toString().trim()
+            val inputWidth = binding.edtWidth.text.toString().trim()
+            val inputHeight = binding.edtHeight.text.toString().trim()
 
             //membuat kondisi untuk mengecek apakah field kosong atau tidak
             var isEmptyFields = false
 
             if (inputLength.isEmpty()) {
                 isEmptyFields = true
-                edtLength.error = "Field ini tidak boleh kosong"
+                binding.edtLength.error = "Field ini tidak boleh kosong"
             }
 
             if (inputWidth.isEmpty()) {
                 isEmptyFields = true
-                edtWidth.error = "Field ini tidak boleh kosong"
+                binding.edtWidth.error = "Field ini tidak boleh kosong"
             }
 
             if (inputHeight.isEmpty()) {
                 isEmptyFields = true
-                edtHeight.error = "Field ini tidak boleh kosong"
+                binding.edtHeight.error = "Field ini tidak boleh kosong"
             }
 
             if (!isEmptyFields) {
                 val volume = inputLength.toDouble() * inputWidth.toDouble() * inputHeight.toDouble()
-                tvResult.text = volume.toString()
+                binding.tvResult.text = volume.toString()
             }
         }
     }
@@ -87,6 +90,6 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         //memanggil fungsi super.onSaveInstanceState(outState)
         super.onSaveInstanceState(outState)
         //menyimpan nilai dari TextView ke key STATE_RESULT
-        outState.putString(STATE_RESULT, tvResult.text.toString())
+        outState.putString(STATE_RESULT, binding.tvResult.text.toString())
     }
 }
